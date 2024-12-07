8af047bac21f1ffce206750da7b049c5896e2f5c
Frans Aris <f.aris@junglegym.com>
2024-12-07 01:36:40 +0100

    Implement label selection and task filtering in App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,6 +13,10 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { client } from "./config/apollo";
 import TaskList from "./components/TaskList";
 import "./index.css";
+import { LabelSelector } from './components/LabelSelector';
+import { useState } from 'react';
+import { useQuery, gql } from '@apollo/client';
+import { LabelManager } from './components/LabelManager';
 
 /**
  * App Component
@@ -29,6 +33,25 @@ import "./index.css";
  * - Implement dark mode toggle
  */
 function App() {
+  const [selectedLabel, setSelectedLabel] = useState<string | null>(null);
+  
+  const GET_TASKS = gql`
+    query GetTasks($labelId: ID) {
+      tasks(where: { labelId: { eq: $labelId } }) {
+        nodes {
+          id
+          title
+          description
+          isCompleted
+          label {
+            id
+            name
+          }
+        }
+      }
+    }
+  `;
+
   return (
     <ApolloProvider client={client}>
       <Router>
@@ -36,10 +59,17 @@ function App() {
         <div className="min-h-screen bg-gray-100">
           {/* Content wrapper with responsive padding */}
           <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+            <div className="mb-6">
+              <LabelSelector 
+                selectedLabel={selectedLabel}
+                onLabelChange={setSelectedLabel}
+              />
+              <LabelManager />
+            </div>
             {/* Route configuration */}
             <Routes>
               {/* Main task list route */}
-              <Route path="/" element={<TaskList />} />
+              <Route path="/" element={<TaskList labelId={selectedLabel} />} />
               {/* TODO: Add additional routes
                   - /login
                   - /register

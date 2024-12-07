a7f6f4d83033754f1685b0b57131d58765a8dad6
Frans Aris <f.aris@junglegym.com>
2024-12-07 01:36:11 +0100

    Enhance Apollo Client configuration with default fetch policies for improved data handling

--- frontend/src/config/apollo.ts
+++ frontend/src/config/apollo.ts
@@ -1,1 +1,18 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client"; export const client = new ApolloClient({ uri: "http://localhost:5001/graphql", cache: new InMemoryCache() });
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+export const client = new ApolloClient({
+  uri: "http://localhost:5001/graphql",
+  cache: new InMemoryCache(),
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: 'network-only',  // Don't cache, always fetch from network
+      nextFetchPolicy: 'network-only'
+    },
+    query: {
+      fetchPolicy: 'network-only'
+    },
+    mutate: {
+      fetchPolicy: 'no-cache'
+    }
+  }
+});

8358543d9c7c76b5b861298faa46381c3a0ed7f9
Frans Aris <f.aris@junglegym.com>
2024-12-07 03:27:48 +0100

    Migration stuff (it gives me migraines for now). Usually my commit messages are better.

---
+++ backend/Migrations/20241207005617_AddTaskStatusAndLastModified.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskManagementAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTaskStatusAndLastModified : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastModified",
+                table: "Tasks",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Tasks",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastModified",
+                table: "Tasks");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Tasks");
+        }
+    }
+}
--- backend/Migrations/20241207010149_RenameTaskStatusToTaskState.Designer.cs
+++ backend/Migrations/20241207010149_RenameTaskStatusToTaskState.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TaskManagementAPI.Data;
 
@@ -10,9 +11,11 @@ using TaskManagementAPI.Data;
 namespace TaskManagementAPI.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241207010149_RenameTaskStatusToTaskState")]
+    partial class RenameTaskStatusToTaskState
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.0");
@@ -22,10 +25,6 @@ namespace TaskManagementAPI.Migrations
                     b.Property<int>("Id")
                         .ValueGeneratedOnAdd()
                         .HasColumnType("INTEGER");
-
-                    b.Property<string>("Color")
-                        .IsRequired()
-                        .HasColumnType("TEXT");
 
                     b.Property<DateTime>("CreatedAt")
                         .HasColumnType("TEXT");
